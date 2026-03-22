d6f98d47a9806000e2ce9417bf662592499a1bbf
Luiz Neto <150538528+luiznetomenezes@users.noreply.github.com>
2026-03-22 13:55:50 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,15 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
+  root: 'client',
   plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './client/src'),
     },
   },
-  server: {
-    middlewareMode: false,
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true,
   },
 })
